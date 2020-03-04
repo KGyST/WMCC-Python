cffeedd925e9e52688c6a069de547296d22fab6a
Gyula Sámuel KARLI <samuel.karli@bimobject.com>
2020-03-04 17:14:51 +0100

    DEV application.py

--- application.py
+++ application.py
@@ -1,55 +1,58 @@
-from flask import Flask, request
+from flask import Flask
+# from flask import Flask, request
 from flask_restful import Resource, Api
-from archicad.WMCC import (
-    createBrandedProduct,
-    buildMacroSet,
-    extractParams,
-)
-
-# @app.route("/teszt")
-# def hello():
-#     return "teszt"
+# from archicad.WMCC import (
+#     createBrandedProduct,
+#     buildMacroSet,
+#     extractParams,
+# )
 
 app = Flask(__name__)
 api = Api(app)
 
 
-class ArchicadEngine(Resource):
+# class ArchicadEngine(Resource):
+#     def get(self):
+#         return {"test": "it's working!"}
+#
+#     def post(self):
+#         data = request.get_json()
+#
+#         result = createBrandedProduct(data)
+#
+#         return result
+#
+#
+# class CreateLCFEngine(Resource):
+#     """
+#     Creating macroset, to be used by internal GDL developers
+#     """
+#     def post(self):
+#         data = request.get_json()
+#         reData = buildMacroSet(data)
+#         #FIXME what to do with older versions of this file?
+#         return reData
+#
+#
+# class ParameterExtractorEngine(Resource):
+#     """
+#     Extracting all parameters from a given GDL object, returning it in json
+#     """
+#     def post(self):
+#         data = request.get_json()
+#         params = extractParams(data)
+#         return params
+
+
+class TestEngine(Resource):
     def get(self):
         return {"test": "it's working!"}
 
-    def post(self):
-        data = request.get_json()
 
-        result = createBrandedProduct(data)
-
-        return result
-
-
-class CreateLCFEngine(Resource):
-    """
-    Creating macroset, to be used by internal GDL developers
-    """
-    def post(self):
-        data = request.get_json()
-        reData = buildMacroSet(data)
-        #FIXME what to do with older versions of this file?
-        return reData
-
-
-class ParameterExtractorEngine(Resource):
-    """
-    Extracting all parameters from a given GDL object, returning it in json
-    """
-    def post(self):
-        data = request.get_json()
-        params = extractParams(data)
-        return params
-
-
-api.add_resource(ArchicadEngine, '/')
-api.add_resource(CreateLCFEngine, '/createlcf')
-api.add_resource(ParameterExtractorEngine, '/extractparams')
+api.add_resource(TestEngine, '/')
+# api.add_resource(ArchicadEngine, '/')
+# api.add_resource(CreateLCFEngine, '/createlcf')
+# api.add_resource(ParameterExtractorEngine, '/extractparams')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
