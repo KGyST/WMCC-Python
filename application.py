1c52d19eaa4d5fb6bf681850cfbe45fe4597b959
Gyula Sámuel KARLI <samuel.karli@bimobject.com>
2020-03-05 15:00:30 +0100

    DEV WMCC

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 
-from src.WMCC import (
+from WMCC import (
     createBrandedProduct,
     buildMacroSet,
     extractParams,
