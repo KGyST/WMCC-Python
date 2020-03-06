94de987e6b23112fcbe3ec1dd543a0467d920d96
Gyula Sámuel KARLI <samuel.karli@bimobject.com>
2020-03-06 10:09:31 +0100

    DEV no PIL

--- application.py
+++ application.py
@@ -1,24 +1,24 @@
-# import os.path
-# from os import listdir
-# import uuid
-# import re
-# import tempfile
-# from subprocess import check_output
-# import shutil
-# import datetime
-# import jsonpickle
-# import multiprocessing as mp
-#
-# import copy
-# import argparse
-#
-# import urllib.request, urllib.parse, urllib.error, json, urllib.parse, os, base64
-# import http.client
-#
+import os.path
+from os import listdir
+import uuid
+import re
+import tempfile
+from subprocess import check_output
+import shutil
+import datetime
+import jsonpickle
+import multiprocessing as mp
+
+import copy
+import argparse
+
+import urllib.request, urllib.parse, urllib.error, json, urllib.parse, os, base64
+import http.client
+
 # from PIL import Image
-# import io
-#
-# from lxml import etree
+import io
+
+from lxml import etree
 
 from flask import Flask, request
 from flask_restful import Resource, Api
