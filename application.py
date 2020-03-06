cec94e3539e25384efc771171d1d482f347e68b9
Gyula Sámuel KARLI <samuel.karli@bimobject.com>
2020-03-06 14:10:23 +0100

    DEV PIL, no lxml

--- application.py
+++ application.py
@@ -25,9 +25,12 @@ api = Api(app)
 
 class TestEngine(Resource):
     def get(self):
-        with Popen(" ".join([os.path.join("archicad", "LP_XMLConverter_18", "LP_XMLConverter.EXE"), "help"]), stdout=PIPE, encoding='utf-8') as _p:
-            _res = _p.stdout.readlines()
-            return {"test": "samu %s" % e.__class__.__name__} if e else _res
+        try:
+            with Popen(" ".join([os.path.join("archicad", "LP_XMLConverter_18", "LP_XMLConverter.EXE"), "help"]), stdout=PIPE, encoding='utf-8') as _p:
+                _res = _p.stdout.readlines()
+                return {"test": "samu %s" % e.__class__.__name__} if e else _res
+        except BaseException as e:
+            print(e.__class__.__name__)
 
 try:
     from subprocess import check_output, Popen, PIPE
