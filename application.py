c9da56d11f1f32005a079c00d76e1fd4ae25040b
Gyula Sámuel KARLI <samuel.karli@bimobject.com>
2020-03-06 14:26:50 +0100

    DEV PIL, no lxml

--- application.py
+++ application.py
@@ -30,7 +30,7 @@ class TestEngine(Resource):
                 _res = _p.stdout.readlines()
                 return {"test": "samu %s" % e.__class__.__name__} if e else _res
         except BaseException as ee:
-            return {"test": "samu %s" % ee.__class__.__name__} if e else _res
+            return {"test": "samu %s" % ee.__class__.__name__} if ee else _res
 
 try:
     from subprocess import check_output, Popen, PIPE
