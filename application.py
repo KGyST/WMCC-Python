52f282c97e10d9e7e59e6582e44d917d991109ea
Gyula Sámuel KARLI <samuel.karli@bimobject.com>
2020-03-06 10:48:15 +0100

    DEV no PIL, yes lxml

--- application.py
+++ application.py
@@ -30,10 +30,10 @@ class TestEngine(Resource):
         return {"test": "samu %s" % e.__class__.__name__ + ": " + e.message}
 
 try:
-    from PIL import Image
+    # from PIL import Image
     from lxml import etree
 except ImportError as e:
-    print(e.__class__.__name__ + ": " + e.message)
+    print(e.__class__.__name__)
 finally:
     api.add_resource(TestEngine, '/')
 
