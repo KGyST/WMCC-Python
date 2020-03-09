53b9b9135cbba3c6a72400fd553cfa1cf9b76bec
Gyula Sámuel KARLI <samuel.karli@bimobject.com>
2020-03-09 09:20:51 +0100

    DEV src

--- application.py
+++ application.py
@@ -30,10 +30,11 @@ class TestEngine(Resource):
         try:
             # return " ".join([a[0] for a in os.walk(".")])
 
-            _p = run(" ".join([os.path.join("archicad", "LP_XMLConverter_18", "LP_XMLConverter.EXE"), "help"]))
+            _p = run(" ".join([os.path.join("src", "archicad", "LP_XMLConverter_18", "LP_XMLConverter.EXE"), "help"]))
             # _res = _p.stderr
             return {"test": "samu %d" % _p.returncode}
         except OSError as ee:
+            print("OSError")
             return {"OSError": "OSError %s" % " ".join([str(a) for a in ee.args])}
         except BaseException as ee:
             return {"test": "BaseException"}
