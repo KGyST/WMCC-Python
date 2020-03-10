6acfebe5b84541470a2c52b233e1c88ed1b59316
Gyula Sámuel KARLI <samuel.karli@bimobject.com>
2020-03-10 09:09:01 +0100

    DEV returncode

--- application.py
+++ application.py
@@ -29,7 +29,8 @@ class TestEngine(Resource):
             with Popen([os.path.join("src", "archicad", "LP_XMLConverter_18", "LP_XMLConverter.EXE"), "help"], stdout=PIPE, stderr=PIPE, stdin=DEVNULL) as proc:
                 _out = proc.stdout.read()
                 _err = proc.stdout.read()
-                return f"Success: {_out} (error: {_err}) "
+                _ret = proc.returncode
+                return f"Success: {_out} (error: {_err}, returncode: {_ret}) "
         except OSError as ex:
             return f"OSError: {ex.__class__.__name__} {ex.__str__()} {ex.errno} {ex.strerror} {ex.filename} {ex.filename2}"
         except BaseException as ex:
