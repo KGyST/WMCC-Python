96a5de9ec01e7f0f6ef6a9de0125972cda17cbd5
Gyula Sámuel KARLI <samuel.karli@bimobject.com>
2020-03-10 11:12:21 +0100

    DEV v8

--- application.py
+++ application.py
@@ -27,10 +27,8 @@ class TestEngine(Resource):
     def get(self):
         try:
             with Popen([os.path.join("src", "archicad", "LP_XMLConverter_18", "LP_XMLConverter.EXE"), "help"], stdout=PIPE, stderr=PIPE, stdin=DEVNULL) as proc:
-                _ret = proc.communicate()
-                _out = _ret[0]
-                _err = _ret[1]
-                _v = 5
+                _out, _err = proc.communicate()
+                _v = 8
                 return f"v{_v} Success: {_out} (error: {_err}) "
         except OSError as ex:
             return f"OSError: {ex.__class__.__name__} {ex.__str__()} {ex.errno} {ex.strerror} {ex.filename} {ex.filename2}"
@@ -39,8 +37,8 @@ class TestEngine(Resource):
 
 try:
     from subprocess import check_output, Popen, PIPE, run, DEVNULL
-    # from PIL import Image
-    # from lxml import etree
+    from PIL import Image
+    from lxml import etree
 finally:
     api.add_resource(TestEngine, '/')
 
