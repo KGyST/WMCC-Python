1d0d774d3973a96be36ee283104d6437bbaa24d3
Gyula Sámuel KARLI <samuel.karli@bimobject.com>
2020-03-10 08:58:04 +0100

    DEV BaseException

--- application.py
+++ application.py
@@ -25,13 +25,15 @@ api = Api(app)
 
 class TestEngine(Resource):
     def get(self):
-        # try:
-        #     with Popen([os.path.join("src", "archicad", "LP_XMLConverter_18", "LP_XMLConverter.EXE"), "help"], stdout=PIPE, stderr=PIPE, stdin=DEVNULL) as proc:
-        #         _out = proc.stdout.read()
-        #         return f"{_out}"
-        # except OSError as ex:
-        #     return f"BaseException: {ex.__class__.__name__} {ex.__str__()} {ex.errno} {ex.strerror} {ex.filename} {ex.filename2}"
-        return "nothing"
+        try:
+            with Popen([os.path.join("src", "archicad", "LP_XMLConverter_18", "LP_XMLConverter.EXE"), "help"], stdout=PIPE, stderr=PIPE, stdin=DEVNULL) as proc:
+                _out = proc.stdout.read()
+                return f"{_out}"
+        except OSError as ex:
+            return f"OSError: {ex.__class__.__name__} {ex.__str__()} {ex.errno} {ex.strerror} {ex.filename} {ex.filename2}"
+        except BaseException as ex:
+            return f"BaseException: {ex.__class__.__name__} {ex.__str__()}"
+
 try:
     from subprocess import check_output, Popen, PIPE, run, DEVNULL
     # from PIL import Image
