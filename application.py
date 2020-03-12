959582de440e8fa09b487ea7e34a42390535dc25
Gyula Sámuel KARLI <samuel.karli@bimobject.com>
2020-03-12 10:44:32 +0100

    DEV v35 opencensus-ext-azure

--- application.py
+++ application.py
@@ -25,7 +25,7 @@ api = Api(app)
 
 class TestEngine(Resource):
     def get(self):
-        _v = 34
+        _v = 35
 
         try:
             with Popen([os.path.join("archicad", "LP_XMLConverter_18", "LP_XMLConverter.EXE"), "help"], stdout=PIPE, stderr=PIPE, stdin=DEVNULL) as proc:
