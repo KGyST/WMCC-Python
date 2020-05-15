3fb17f5d3d73c2e28c8421aa2614332fc0a7636f
Karli Gyula Sámuel <samuel.karli@bimobject.com>
2020-05-15 13:49:45 +0200

    DEV -Web.config fix

--- src/WMCC.py
+++ src/WMCC.py
@@ -1964,6 +1964,9 @@ def startConversion(targetGDLDirName = TARGET_GDL_DIR_NAME, sourceImageDirName='
         _out, _err = proc.communicate()
         logging.info(f"Success: {_out} (error: {_err}) ")
 
+        if "rror" in _out:
+            logging.error(f"While compiling: {_out}")
+
     # cleanup ops
     if CLEANUP:
         if _picdir:
