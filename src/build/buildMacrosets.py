3a8f397b343415a8f644a213354815dc6352f930
Karli Gyula Sámuel <samuel.karli@bimobject.com>
2020-06-10 14:48:21 +0200

    DEV

--- src/build/buildMacrosets.py
+++ src/build/buildMacrosets.py
@@ -4,10 +4,16 @@ import json
 import datetime
 import logging
 
-with open(WMCC.APP_CONFIG, "r") as ac:
-    appJSON                     = json.load(ac)
-    CONTENT_DIR_NAME            = appJSON["CONTENT_DIR_NAME"]
-    CATEGORY_DATA_JSON          = os.path.join(CONTENT_DIR_NAME, "categoryData.json")
+TEST  = bool(os.environ['TEST']) if "TEST" in os.environ else False
+
+if "CONTENT_DIR_NAME" in os.environ:
+    CONTENT_DIR_NAME = os.environ["CONTENT_DIR_NAME"]
+else:
+    with open(WMCC.APP_CONFIG, "r") as ac:
+        appJSON                     = json.load(ac)
+        CONTENT_DIR_NAME            = appJSON["CONTENT_DIR_NAME"]
+
+CATEGORY_DATA_JSON          = os.path.join(CONTENT_DIR_NAME, "categoryData.json")
 
 data = {}
 
@@ -15,7 +21,7 @@ with open(CATEGORY_DATA_JSON, "r") as cD:
     categoryData = json.load(cD)
 
     for cat in categoryData:
-        if cat in ("tests", ):
+        if TEST == (cat in ("tests", )):
             logging.info(f"Building category: {cat}")
             data["category"] = cat
 
